9daf3027d029919b0a616c18dc930d1e75af34b7
Cbas12 <105694795+Cbas12@users.noreply.github.com>
2024-03-19 04:52:47 +0000

    fin

---
+++ check.py
@@ -0,0 +1,8 @@
+import pyspark
+from pyspark.sql import SparkSession
+
+print("Hello!")
+
+
+#export PYTHONPATH="${SPARK_HOME}/python/lib/py4j-0.10.9.7-src.zip:$PYTHONPATH"
+#py4j-0.10.9.7
---
+++ kafka_spark.py
@@ -0,0 +1,53 @@
+from kafka import KafkaProducer
+import json
+import time
+import os
+import pyspark
+from pyspark.sql import SparkSession
+
+
+def json_serializer(data):
+    return json.dumps(data).encode('utf-8')
+
+server = 'localhost:9092'
+
+producer = KafkaProducer(
+    bootstrap_servers=[server],
+    value_serializer=json_serializer
+)
+
+pyspark_version = pyspark.__version__
+kafka_jar_package = f"org.apache.spark:spark-sql-kafka-0-10_2.12:{pyspark_version}"
+
+spark = SparkSession \
+    .builder \
+    .master("local[*]") \
+    .appName("GreenTripsConsumer") \
+    .config("spark.jars.packages", kafka_jar_package) \
+    .getOrCreate()
+
+print(producer.bootstrap_connected())
+
+green_stream = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "green-trips") \
+    .option("startingOffsets", "earliest") \
+    .load()
+
+print("Green stream")
+print(green_stream)
+
+print("Peek:")
+def peek(mini_batch, batch_id):
+    first_row = mini_batch.take(1)
+
+    if first_row:
+        print(first_row[0])
+
+query = green_stream.writeStream.foreachBatch(peek).start()
+
+print(query)
+
+query.stop()
